c0f79d9faff5b7a1c48ee6823abb24f99f880899
David Paun <davidencu@debian>
2025-04-10 14:48:12 -0400

    Created the remove_hunt function

--- treasure_manager.c
+++ treasure_manager.c
@@ -31,18 +31,54 @@ void get_cwd_name(char *cwd,char p2[][100])
   }
 }
 
+void find_treasure_hunt(DIR *d,int *treasure_hunt_found,char *hunt_id, char *relative_path, int *directory_number)
+{
+  *directory_number=0;
+  relative_path[0]='\0';
+  struct dirent *current_pointer=readdir(d);
+  while(current_pointer != NULL) //If there is no directory inside Proiect_SO
+  {
+    if (current_pointer->d_type == DT_DIR && strcmp(current_pointer->d_name, ".git")!=0 && strcmp(current_pointer->d_name, ".")!=0 && strcmp(current_pointer->d_name, "..")!=0)
+    {
+      DIR *subd=opendir(current_pointer->d_name);
+      struct dirent *current_pointer_subd=readdir(subd);
+      
+      if(subd!=NULL)
+      {
+        while(current_pointer_subd != NULL)
+        {
+          printf("%s - %s\n", current_pointer_subd->d_name, hunt_id);
+          if (current_pointer_subd->d_type == DT_DIR && strcmp(current_pointer_subd->d_name, hunt_id)==0)
+          {
+            *treasure_hunt_found=1;
+            sprintf(relative_path,"./%s/%s",current_pointer->d_name,hunt_id);
+            break;
+          }
+          current_pointer_subd = readdir(subd);
+        }
+      }
+      *directory_number=*directory_number+1;
+      if(closedir(subd)!=0)
+      {
+        perror("closedir");
+        exit(-1);
+      }
+    }
+    current_pointer=readdir(d);
+  }
+  printf("%s\n",relative_path);
+}
+
 void add_function(char *hunt_id)
 {
   char p2[100];
-  treasure treasures[MAX_TREASURES];
   char cwd[1000];
-  struct dirent *current_pointer=NULL;
+  //struct dirent *current_pointer=NULL;
   getcwd(cwd, sizeof(cwd));
   get_cwd_name(cwd,&p2);
   printf("%s\n",p2);
   DIR *d=NULL;
   int directory_number=0;
-  char directory_relative_path[100];
   int treasure_hunt_found=0;
   if((d=opendir("."))==NULL) //if the Proiect_SO directory somehow does not exist
   {
@@ -60,52 +96,23 @@ void add_function(char *hunt_id)
   {
     printf("Entered Proiect_SO directory\n");
   }
-  current_pointer=readdir(d);
-  while(current_pointer != NULL) //If there is no directory inside Proiect_SO
-  {
-    if (current_pointer->d_type == DT_DIR && strcmp(current_pointer->d_name, ".git")!=0 && strcmp(current_pointer->d_name, ".")!=0 && strcmp(current_pointer->d_name, "..")!=0)
-    {
-      DIR *subd=opendir(current_pointer->d_name);
-      struct dirent *current_pointer_subd=readdir(subd);
-      
-      if(subd!=NULL)
-      {
-        while(current_pointer_subd != NULL)
-        {
-          printf("%s - %s\n", current_pointer_subd->d_name, hunt_id);
-          if (current_pointer_subd->d_type == DT_DIR && strcmp(current_pointer_subd->d_name, hunt_id)==0)
-          {
-            treasure_hunt_found=1;
-            sprintf(directory_relative_path,"./%d/%s",directory_number,hunt_id);
-            break;
-          }
-          current_pointer_subd = readdir(subd);
-        }
-      }
-      directory_number++;
-      if(closedir(subd)!=0)
-      {
-        perror("closedir");
-        exit(-1);
-      }
-    }
-    current_pointer=readdir(d);
-  }
+  char relative_path[100];
+  find_treasure_hunt(d, &treasure_hunt_found, hunt_id, relative_path, &directory_number);
   if(treasure_hunt_found == 0)
   {
-    sprintf(directory_relative_path,"./%d",directory_number);
-      if(mkdir(directory_relative_path, 0777)==-1)
+    sprintf(relative_path,"./%d",directory_number);
+      if(mkdir(relative_path, 0777)==-1)
       {
-        printf("%s\n", strerror(errno));
+        perror("mkdir");
         exit(-1);
       }
       else
       {
         printf("Succesfully created directory no %d\n", directory_number);
       }
-      strcat(directory_relative_path,"/");
-      strcat(directory_relative_path,hunt_id);
-      if(mkdir(directory_relative_path, 0777)==-1)
+      strcat(relative_path,"/");
+      strcat(relative_path,hunt_id);
+      if(mkdir(relative_path, 0777)==-1)
       {
         printf("Cannot add the hunt:%s\n", strerror(errno));
         exit(-1);
@@ -115,11 +122,9 @@ void add_function(char *hunt_id)
         printf("Succesfully added the hunt\n");
       }
   }
-  else
-  {
     treasure buffer;
-    strcat(directory_relative_path,"/file.bin");
-    int descr=open(directory_relative_path,O_CREAT | O_WRONLY | O_APPEND | O_TRUNC);
+    strcat(relative_path,"/file.bin");
+    int descr=open(relative_path,O_CREAT | O_WRONLY | O_APPEND, 00755);
     if(descr == -1)
     {
       perror("open");
@@ -141,8 +146,6 @@ void add_function(char *hunt_id)
       perror("close");
       exit(-1);
     }
-
-  }
   if(closedir(d)!=0)
   {
     perror("closedir");
@@ -151,6 +154,89 @@ void add_function(char *hunt_id)
   else
   {
     printf("Succesfully exited the Proiect-SO directory\n");
+  }
+}
+
+void re_move(char *hunt_id,char *treasure_id)
+{
+  treasure treasures[MAX_TREASURES];
+  printf("%ld\n",sizeof(treasures)/MAX_TREASURES);
+  int treasures_number=0;
+  int directory_number=0;
+  DIR *d=NULL;
+  char relative_path[100];
+  int treasure_hunt_found=0;
+  if ((d=opendir("."))==NULL)
+  {
+    perror("opendir");
+    exit(-1);
+  }
+  find_treasure_hunt(d, &treasure_hunt_found, hunt_id, relative_path, &directory_number);
+  if(treasure_hunt_found)
+  {
+    strcat(relative_path,"/file.bin");
+    int descr=open(relative_path, O_RDONLY);
+    if(descr<0)
+    {
+      perror("open");
+      exit(-1);
+    }
+    else
+    {
+      while((read(descr,&treasures[treasures_number], sizeof(treasures)/MAX_TREASURES)) > 0)
+      {
+        treasures_number++;
+      }
+      for(int i=0;i<treasures_number;i++)
+      {
+        if(strcmp(treasures[i].id, treasure_id)==0)
+        {
+          for (int j=i;j<treasures_number-1;j++)
+          {
+            treasures[i]=treasures[i+1];
+          }
+          treasures_number--;
+        }
+      }
+    }
+    if(close(descr)!=0)
+    {
+      perror("close");
+      exit(-1);
+    }
+    if(open(relative_path, O_TRUNC) < 0)
+    {
+      perror("open");
+      exit(-1);
+    }
+    if(close(descr)!=0)
+    {
+      perror("close");
+      exit(-1);
+    }
+    if(open(relative_path, O_WRONLY | O_APPEND) < 0)
+    {
+      perror("open");
+      exit(-1);
+    }
+    for(int i=0;i<treasures_number;i++)
+    {
+      if(write(descr,&treasures[i],sizeof(treasures)/MAX_TREASURES)==-1)
+      {
+        perror("write");
+        exit(-1);
+      }
+    }
+    if(close(descr)!=0)
+    {
+      perror("close");
+      exit(-1);
+    }
+  }
+  if (closedir(d)!=0)
+  {
+    perror("closedir");
+    exit(-1);
   }
 }
 
@@ -196,6 +282,7 @@ int main(int argc,char **argv)
      case 3:
        break;
      case 4:
+        re_move(argv[2], argv[3]);
        break;
      case 5:
        break;
